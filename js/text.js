4db8eb137533bbb236392fc30c0d55423f1f9e4f
stelonek123 <woznystefangry@gmail.com>
2025-10-28 21:21:49 +0100

    Update text.js
    changed file download line

--- js/text.js
+++ js/text.js
@@ -18,10 +18,11 @@ function Tab() {
     })
     BOp2.addEventListener("click", function(event) {
         event.preventDefault();
-        site_content.innerHTML = 'Download XWB for Windows: <a href="xwbsetup/xwb_winsetup.exe">Click Here</a><br> You are downloading Xilan Web Browser, thanks mate!';
+        site_content.innerHTML = 'Download XWB for Windows: <a href="https://www.mediafire.com/file/nf39coh9nn3h0it/xwb_winsetup.exe/file">Click Here</a><br> You are downloading Xilan Web Browser, thanks mate!';
     })
     BOp3.addEventListener("click", function(event) {
         event.preventDefault();
         site_content.innerHTML = '<header class="FeaturesHeader">Features</header><br><header class="FeaturesHeader">1. Lite-weight</header>This browser can be used on low-end PCs, it s easy, but it s only first version, there will be more functions, its just about time.<br><br><header class="FeaturesHeader">2. Language</header>In two senses: basicaly the language of browser is english. Other Means that was coded with Python\PyQT5, easy programer language with simple syntax<br><br></br>';
     })
+
 }
