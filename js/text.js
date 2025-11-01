6020b151945d64e0a328818b073d361d6bfd6885
stelonek123 <woznystefangry@gmail.com>
2025-11-01 16:33:10 +0100

    Update text.js

--- js/text.js
+++ js/text.js
@@ -11,15 +11,15 @@ function Tab() {
     //site content
     const site_content = document.getElementById("site-content")
 
-    site_content.append("Xilan Web Browser (or XWB) is a simple web browser developed by Xilan Studio(Pefek45), it was created mainly for low end PCs. This is cause of low count of functions, but remember, this is only the beggining. We are gonna add more functions, like tabs or local password saving.");
+    site_content.append("Xilan Web Browser (or XWB) is a simple web browser developed by Xilan Studio(Pefek45), it was created mainly for low end PCs. This is cause of low count of functions, but remember, this is only the beggining. We are gonna add more functions, like local password saving.");
 
     BOp1.addEventListener("click", function(event) {
         event.preventDefault();
-        site_content.innerHTML = '<header class="FeaturesHeader">Whats New?</header> <br>(0.8) Honestly Navigation Bar now have icons<br> Added simple tab system (double click on tabbar to add new tab), <br> Migrated project to PySide6 <br><br> (0.9p1) App Icon changed'
+        site_content.innerHTML = '<header class="FeaturesHeader">Whats New?</header> <br>(0.8) Honestly Navigation Bar now have icons<br> Added simple tab system (double click on tabbar to add new tab), <br> Migrated project to PySide6 <br><br> (0.9p1) App Icon changed, <br><br> (0.9p2) -Added first settings[New tab page, Dark mode]<br>-Added Tab Button'
     })
     BOp2.addEventListener("click", function(event) {
         event.preventDefault();
-        site_content.innerHTML = 'Download XWB for Windows(0.9p1): <a href="https://www.mediafire.com/file/osyjwgaw62qejz2/xwb_winsetup_0.9p1.exe/file" target="_blank">Click Here</a><br> You are downloading Xilan Web Browser, thanks mate!';
+        site_content.innerHTML = 'Download XWB for Windows(0.9p1): <a href="https://www.mediafire.com/file/osyjwgaw62qejz2/xwb_winsetup_0.9p1.exe/file" target="_blank">Click Here</a><br> Download XWB for Windows(0.9p2): <a href="https://www.mediafire.com/file/k2f60ouh5ekjmsi/xwb_winsetup09p2.exe/file" target="_blank">Click Here</a><br> You are downloading Xilan Web Browser, thanks mate!';
     })
     BOp3.addEventListener("click", function(event) {
         event.preventDefault();
@@ -27,9 +27,10 @@ function Tab() {
     })
     BOp4.addEventListener("click", function(event) {
         event.preventDefault();
-        site_content.innerHTML = '<header class="FeaturesHeader">Update Log</header><br><header class="FeaturesHeader">27.10.2025 - 0.4 - Code update</header><br><br><header class="FeaturesHeader">28.10.2025 - 0.6 - PySide6 Migration</header><br><br><header class="FeaturesHeader">29.10.2025 - 0.8 - Tabs Update</header><br><br><header class="FeaturesHeader">29.10.2025 - 0.9p1 - Icon Update</header>';
+        site_content.innerHTML = '<header class="FeaturesHeader">Update Log</header><br><header class="FeaturesHeader">27.10.2025 - 0.4 - Code update</header><br><br><header class="FeaturesHeader">28.10.2025 - 0.6 - PySide6 Migration</header><br><br><header class="FeaturesHeader">29.10.2025 - 0.8 - Tabs Update</header><br><br><header class="FeaturesHeader">29.10.2025 - 0.9p1 - Icon Update</header><br><br><header class="FeaturesHeader">31.10.2025 - 0.9p2 - Settings Added(Dark/Light mode, Choose new tab site), added Tab button</header>';
     })
 
 }
+
 
 
